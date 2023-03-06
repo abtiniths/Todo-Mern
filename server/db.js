e912fbd607a8700c7eeeeb11b9cc4ed149f17946
abtiniths <abtin.nouran@iths.se>
2023-03-06 10:12:58 +0100

    fix

--- server/db.js
+++ server/db.js
@@ -5,6 +5,6 @@ module.exports = async () => {
     mongoose.connect("mongodb://localhost/todo-app");
     console.log("Connected to database.");
   } catch (error) {
-    console.log("Could not connect to database.", error);
+    console.log("Could not connect to database...", error);
   }
 };
